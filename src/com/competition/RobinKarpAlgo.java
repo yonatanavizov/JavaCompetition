61511b0374a69c7f0a8b0afc234cae0c54a75e52
IronAsaf <asafdangoor@gmail.com>
2021-12-10 15:30:43 +0200

    Added Robin Karp java class

---
+++ src/com/competition/RobinKarpAlgo.java
@@ -0,0 +1,19 @@
+package com.competition;
+
+import java.util.List;
+
+public class RobinKarpAlgo implements ISearchAlgoFamily {
+
+	@Override
+	public SearchResult Search(String a1, String a2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> GetFoundResults() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
