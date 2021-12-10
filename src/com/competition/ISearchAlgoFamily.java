ceb45bdafb497392464320be72db37371dcd1acb
IronAsaf <asafdangoor@gmail.com>
2021-12-10 14:08:34 +0200

    Added KMP and Interface, basis of Naive Search

---
+++ src/com/competition/ISearchAlgoFamily.java
@@ -0,0 +1,14 @@
+package com.competition;
+import java.util.List;
+public interface ISearchAlgoFamily
+{
+	    enum SearchResult
+	    {
+	        Found,
+	        ExactlyTheSame,
+	        FoundMany,
+	        NotFound
+	    }
+	    SearchResult Search(String a1,String a2);
+	    List<Integer> GetFoundResults();
+}
---
+++ src/com/competition/NaiveSearchAlgo.java
@@ -0,0 +1,28 @@
+package com.competition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NaiveSearchAlgo implements ISearchAlgoFamily {
+
+	private List<Integer> results;
+	
+	public NaiveSearchAlgo()
+	{
+		results = new ArrayList<Integer>();
+	}
+	
+	
+	@Override
+	public SearchResult Search(String a1, String a2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> GetFoundResults() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
