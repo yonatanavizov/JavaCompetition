8fe680cfc36fabf4b46ff7fd4bf29edb435b9858
IronAsaf <asafdangoor@gmail.com>
2022-01-13 19:10:50 +0200

    Test Unit

---
+++ src/com/competition/contest/test/ContestServiceTest.java
@@ -0,0 +1,60 @@
+package com.competition.contest.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import com.competition.contest.Contest;
+import com.competition.contest.ContestService;
+import com.competition.match.Match;
+import com.competition.match.Match.OutCome;
+import com.competition.team.Team;
+import com.competition.team.Team.Rank;
+
+public class ContestServiceTest
+{
+	private HashMap<String, Contest> testDb;
+	private HashMap<String, Contest> requestedDb;
+	
+	@BeforeAll
+	public static void beforeAll()
+	{
+		System.out.println("Starting test...");
+	}
+	
+	@AfterAll
+	public static void afterAll()
+	{
+		System.out.println("Ending test...");
+	}
+	
+	public void Init()
+	{
+		testDb = new HashMap<String, Contest>();
+		Rank r = Rank.ADVANCED;
+		Team toAdd = new Team(0, "first team", "basket ball",r,1,11,(float) 0.11, "A lot of summaryfor ther year 1976 and all that .");
+		Team toAdd2 = new Team(1, "second team", "basket ball",r,2,22,(float) 0.22, "A different amount of lot of summary 1976 and all that.");
+		Match m1 = new Match(toAdd, toAdd2, 0);
+		m1.set_outcome(OutCome.Team_A_Won);
+		Contest c1 = new Contest();
+		HashMap<String, ArrayList<Match>> toAddToCon = new HashMap<String, ArrayList<Match>>();
+		toAddToCon.put("Start", new ArrayList<Match>());
+		
+		testDb.put("0", c1);
+		requestedDb = ContestService.get_instance().get_objects();
+	}
+	
+	@Test
+	public void TestContest()
+	{
+		Init();
+		//Assert - Our DB vs the JSON db.
+		assertFalse(testDb.equals(requestedDb));
+	}
+}
